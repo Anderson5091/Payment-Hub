0834d34d1e1d488947a5a94fd4a04ee23866a8fb
Anderson5091 <Anderson5091@users.noreply.github.com>
2026-01-01 22:37:35 -0500

    feat: add HMAC secret and upload max size configuration to app config

--- config/app.php
+++ config/app.php
@@ -1,3 +1,6 @@
-'hmac_secret' => env('HMAC_SECRET'),
+<?php
 
-'upload_max_size' => env('UPLOAD_MAX_SIZE', 5120),
+return [
+    'hmac_secret' => env('HMAC_SECRET'),
+    'upload_max_size' => env('UPLOAD_MAX_SIZE', 5120),
+];
